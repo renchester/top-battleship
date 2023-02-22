8e4e5adc32243e095dee96c4b2c33b9d52bab187
Renchester Ramos <renchesterjramos@gmail.com>
2023-02-23 00:29:16 +0800

    Refactor player tests and clean code

--- src/scripts/__tests__/player.test.js
+++ src/scripts/__tests__/player.test.js
@@ -3,111 +3,77 @@
 import { Player, Ship, Board } from '../model';
 
 describe('player', () => {
-  test('checks if computer can successfully place multiple ships inside board', () => {
-    const ai = Player('computer');
+  let player1;
+  let player2;
 
-    ai.ships = [
+  beforeEach(() => {
+    player1 = Player('human');
+    player2 = Player('computer');
+  });
+
+  test('checks if computer can successfully place multiple ships inside board', () => {
+    player1.ships = [
       Ship(5, 'carrier'),
       Ship(4, 'battleship'),
       Ship(3, 'destroyer'),
     ];
 
-    ai.placeComputerShips();
+    player1.placeComputerShips();
 
     // filter out board ship names
-    const aiShipsOnBoard = [
+    const shipsOnBoard = [
       ...new Set(
-        ai.board.state
+        player1.board.state
           .map((square) => {
             if (!square.shipName) return 0;
-
-            // eslint-disable-next-line consistent-return
             return square.shipName;
           })
           .filter((shipName) => shipName),
       ),
     ];
 
-    expect(aiShipsOnBoard.length).toBe(3);
-    expect(aiShipsOnBoard).toContain('carrier');
-    expect(aiShipsOnBoard).toContain('destroyer');
-    expect(aiShipsOnBoard).toContain('battleship');
-
-    const ai2 = Player('computer2');
-
-    ai2.ships = [
-      Ship(4, 'ship--1'),
-      Ship(8, 'ship--2'),
-      Ship(4, 'ship--3'),
-      Ship(5, 'ship--4'),
-      Ship(6, 'ship--5'),
-      Ship(7, 'ship--6'),
-    ];
-
-    ai2.placeComputerShips();
-
-    const ai2ShipsOnBoard = [
-      ...new Set(
-        ai2.board.state
-          .map((square) => {
-            if (!square.shipName) return 0;
-
-            // eslint-disable-next-line consistent-return
-            return square.shipName;
-          })
-          .filter((shipName) => shipName),
-      ),
-    ];
-
-    expect(ai2ShipsOnBoard.length).toBe(6);
+    expect(shipsOnBoard).toHaveLength(3);
+    expect(shipsOnBoard).toContain('carrier');
+    expect(shipsOnBoard).toContain('destroyer');
+    expect(shipsOnBoard).toContain('battleship');
   });
 
   test('computer generates a random attack', () => {
-    const human = Player('you');
-    const ai = Player('computer');
+    player1.board = Board(3);
+    player1.ships = [Ship(2)];
+    player1.board.placeShip([2, 2], player1.ships[0]);
+    player2.generateAttack(player1);
 
-    human.board = Board(3);
-    human.ships = [Ship(2)];
-    human.board.placeShip([2, 2], human.ships[0]);
-    ai.generateAttack(human);
-
-    expect(human.board.state.find((square) => square.isHit)).toBeTruthy();
+    expect(player1.board.state.find((square) => square.isHit)).toHaveProperty(
+      'isHit',
+      true,
+    );
   });
 
   test.skip('computer follows natural patterns when attacking', () => {});
 
   test('checks if square is successfully hit after attack', () => {
-    const player1 = Player('one');
-    const player2 = Player('two');
-
-    // Expect player to have board on init
-
     player2.ships.push(Ship(2, 'patroller'));
     player2.board.placeShip([1, 1], player2.ships[0]);
 
     player1.attack(player2, [1, 1]);
 
-    expect(player2.board.state[0].isHit).toBeTruthy();
+    expect(player2.board.state[0].isHit).toBe(true);
   });
 
   test('returns when trying to attack an already hit square', () => {
-    const player1 = Player('one');
-    const player2 = Player('two');
-
     player2.ships.push(Ship(2, 'patroller'));
     player2.board.placeShip([1, 1], player2.ships[0]);
 
     player1.attack(player2, [1, 1]);
 
-    expect(player1.attack(player2, [1, 1])).toBeFalsy();
+    expect(player1.attack(player2, [1, 1])).toBe(false);
   });
 
   test('checks if square with ship loses hitPoints after receiving attack', () => {
-    const player1 = Player('one');
-    const player2 = Player('two');
-
     player2.ships.push(Ship(2, 'patroller'));
     player2.board.placeShip([1, 1], player2.ships[0]);
+
     let origHP = player2.ships[0].hitPoints;
 
     player1.attack(player2, [1, 1]);
