be1e373c7716eb84bd9aaaf9d9edb0c07884d84f
Renchester Ramos <renchesterjramos@gmail.com>
2023-02-23 00:16:10 +0800

    Refactor tests and clean code

--- src/scripts/__tests__/board.test.js
+++ src/scripts/__tests__/board.test.js
@@ -1,106 +1,103 @@
 /* eslint-disable no-undef */
 
-import { Board, Ship, Player } from '../model';
+import { Board, Ship } from '../model';
 
 describe('board', () => {
+  let sampleBoard;
+
+  beforeEach(() => {
+    sampleBoard = Board(3);
+  });
+
   test('creates a grid with the same height and width', () => {
-    expect(Board(10).size).toBe(100);
+    expect(sampleBoard.size).toBe(9);
   });
 
   test('creates squares with correct row and column numbers', () => {
-    expect(Board(10).state[0].row).toBe(1);
-    expect(Board(10).state[0].column).toBe(1);
-    expect(Board(10).state[19].row).toBe(2);
-    expect(Board(3).state[4].row).toBe(2);
-    expect(Board(3).state[4].column).toBe(2);
-    expect(Board(3).state[8].row).toBe(3);
-    expect(Board(3).state[8].column).toBe(3);
+    expect(sampleBoard.state[4].row).toBe(2);
+    expect(sampleBoard.state[4].column).toBe(2);
+    expect(sampleBoard.state[8].row).toBe(3);
+    expect(sampleBoard.state[8].column).toBe(3);
   });
 
   test('creates squares with correct squareID numbers', () => {
-    expect(Board(10).state[0].id).toBe(1);
-    expect(Board(10).state[99].id).toBe(100);
-    expect(Board(10).state[49].id).toBe(50);
-    expect(Board(3).state[8].id).toBe(9);
-    expect(Board(2).state[1].id).toBe(2);
+    expect(sampleBoard.state[8].id).toBe(9);
   });
 
   test('should be able to place ships at specific coordinates', () => {
-    const sampleBoard = Board(3);
-    const sampleShip = Ship(2, 'destroyer');
+    sampleBoard.placeShip([2, 1], Ship(2, 'destroyer'));
 
-    sampleBoard.placeShip([2, 1], sampleShip);
-
-    expect(sampleBoard.state[3].hasShip).toBeTruthy();
-    expect(sampleBoard.state[6].hasShip).toBeTruthy();
+    expect(sampleBoard.state[3].hasShip).toBe(true);
+    expect(sampleBoard.state[6].hasShip).toBe(true);
     expect(sampleBoard.state[6].shipName).toBe('destroyer');
   });
 
   test('fills up correct coordinates when placing a ship', () => {
-    const sampleBoard = Board(3);
-
     const horizontalShip = Ship(2, 'patroller');
     const verticalShip = Ship(3, 'destroyer');
 
-    // Default vertical
     horizontalShip.orientation = 'horizontal';
     verticalShip.orientation = 'vertical';
 
     sampleBoard.placeShip([1, 1], horizontalShip);
     sampleBoard.placeShip([1, 3], verticalShip);
 
-    expect(sampleBoard.state[0].hasShip).toBeTruthy();
-    expect(sampleBoard.state[1].hasShip).toBeTruthy();
-    expect(sampleBoard.state[0].hasShip).toEqual(sampleBoard.state[1].hasShip);
+    // horizontal ship
+    expect(sampleBoard.state[0].hasShip).toBe(true);
+    expect(sampleBoard.state[1].hasShip).toBe(true);
 
-    expect(sampleBoard.state[2].hasShip).toBeTruthy();
-    expect(sampleBoard.state[5].hasShip).toBeTruthy();
-    expect(sampleBoard.state[8].hasShip).toBeTruthy();
-    expect(sampleBoard.state[2].hasShip).toEqual(sampleBoard.state[5].hasShip);
-    expect(sampleBoard.state[5].hasShip).toEqual(sampleBoard.state[8].hasShip);
+    // vertical ship
+    expect(sampleBoard.state[2].hasShip).toBe(true);
+    expect(sampleBoard.state[5].hasShip).toBe(true);
+    expect(sampleBoard.state[8].hasShip).toBe(true);
   });
 
   test('returns error/false when placing ship on a filled square', () => {
-    const sampleBoard = Board(3);
-    const sampleShip = Ship(2);
-    const sampleShip2 = Ship(3);
+    sampleBoard.placeShip([1, 3], Ship(2));
 
-    sampleBoard.placeShip([1, 3], sampleShip);
-
-    expect(() => sampleBoard.placeShip([2, 3], sampleShip2)).toThrow();
+    expect(sampleBoard.placeShip([2, 3], Ship(3))).toBe(false);
   });
 
-  test('throws error when placing ship outside board', () => {
-    const sampleBoard = Board(3);
-    const sampleShip = Ship(2);
-
-    expect(() => sampleBoard.placeShip([3, 3], sampleShip)).toThrow();
+  test('returns error/false when placing ship outside board', () => {
+    expect(sampleBoard.placeShip([3, 3], Ship(2))).toBe(false);
   });
 
   test('finds correct square using square id number', () => {
-    const sampleBoard = Board(3);
-
     expect(sampleBoard.findSquareWithID(3)).toEqual(sampleBoard.state[2]);
   });
 
   test('finds correct square using row and column number', () => {
-    const sampleBoard = Board(3);
-
     expect(sampleBoard.findSquareWithRowCol([2, 2])).toEqual(
       sampleBoard.state[4],
     );
   });
 
   test('hits all surrounding squares around ship when it is sunk', () => {
-    const player1 = Player('one');
-    const player2 = Player('two');
+    sampleBoard.placeShip([2, 2], Ship(1));
+    sampleBoard.explodeShip([2, 2]);
+    expect(sampleBoard.state.every((square) => square.isHit)).toBe(true);
+  });
 
-    player2.ships.push(Ship(1, 'patroller'));
-    player2.board = Board(3);
-    player2.board.placeShip([2, 2], player2.ships[0]);
+  test('explodes squares around multiple ships', () => {
+    sampleBoard = Board(10);
+    sampleBoard.placeShip([1, 1], Ship(5, 'carrier'));
+    sampleBoard.placeShip([5, 5], Ship(2, 'patroller'));
 
-    player1.attack(player2, [2, 2]);
+    sampleBoard.explodeShip([1, 1]);
+    sampleBoard.explodeShip([5, 5]);
 
-    expect(player2.board.state.every((square) => square.isHit)).toBeTruthy();
+    expect(sampleBoard.state.filter((square) => square.isHit).length).toEqual(
+      24,
+    );
+  });
+
+  test('exploding works even on overlapping squares', () => {
+    sampleBoard.placeShip([2, 1], Ship(1, 'bomb1'));
+    sampleBoard.placeShip([2, 3], Ship(1, 'bomb2'));
+
+    sampleBoard.explodeShip([2, 1]);
+    sampleBoard.explodeShip([2, 3]);
+
+    expect(sampleBoard.state.every((square) => square.isHit)).toBe(true);
   });
 });
